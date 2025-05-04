bdc159d84b60ef0c7989e304b6c91143b6ec2c0c
Scott Haley <williamscotthaley@gmail.com>
2025-05-03 18:11:32 -0700

    add react router

---
+++ frontend/src/routes/Home.tsx
@@ -0,0 +1,21 @@
+import FeaturedBooks from "../components/FeaturedBooks";
+
+function Home() {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="bg-blue-600 text-white py-4 px-8 shadow-lg w-full">
+        <h1 className="text-lg">The Library</h1>
+      </div>
+      <div className="max-w-[1000px] w-full">
+        <div className="my-16 bg-gradient-to-r from-violet-600 to-fuchsia-400 rounded-xl p-8">
+          <h2 className="text-white text-2xl font-bold">
+            Discover Your Next Adventure
+          </h2>
+        </div>
+        <FeaturedBooks />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
